3cd52c6758243cb0fc02b4f23b32dccaba2c995a
Barbarina <barbaramarinaufsm@gmail.com>
2022-05-20 19:56:05 -0300

    feat: add authorizations endpoint

---
+++ src/middlewares/tokenMiddleware.js
@@ -0,0 +1,23 @@
+import db from "../db.js";
+
+export default async function validateToken(req, res, next) {
+    const authorization = req.headers.authorization;
+    const token = authorization?.replace("Bearer ", "").trim();
+
+    if (!token) return res.status(401).send("Token not received.");
+
+    try {
+        const session = await db.collection("Sessions").findOne({token});
+        if (!session) return res.status(401).send("This session does not exist.");
+
+        const user = await db.collection("Users").findOne({_id: session.userId});
+        if (!user) return res.status(401).send("This user does not exist");
+
+        res.locals.user = user;
+    } catch(e) {
+        console.log(e);
+        return res.sendStatus(500);
+    }
+
+    next();
+}
