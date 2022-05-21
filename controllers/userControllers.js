17e18a9a3826d435725fa162dda6003fe08ee678
Barbarina <barbaramarinaufsm@gmail.com>
2022-05-20 23:12:11 -0300

    feat: add get questions endpoint

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,23 @@
-// import { getQuestions } from "../database/actions.js";
+import { getQuestions } from "../database/actions.js";
 
 export async function getUserData(_req, res) {
     const { user } = res.locals;
-    res.status(200).send(user);
+    try {
+        res.status(200).send(user);
+    } catch (e) {
+        console.log(e);
+        return res.sendStatus(500);
+    }
+    
+}
+
+export async function getTheQuestions(_req, res) {
+    try {
+        const questions = await getQuestions();
+        
+        res.status(200).send(questions);
+    } catch (e) {
+        console.log(e);
+        return res.sendStatus(500);
+    }
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUserData } from "../controllers/userControllers.js";
+import { getTheQuestions, getUserData } from "../controllers/userControllers.js";
 import validateToken  from "../middlewares/tokenMiddleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/user", validateToken, getUserData)
+userRouter.get("/user", validateToken, getUserData);
+userRouter.get("/questions", validateToken, getTheQuestions);
 
 export default userRouter;
